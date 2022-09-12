c85f0ca522f9f85bc3ce819c4a965baafb2b2c22
Elizav555 <lizagarkina5@gmail.com>
2022-09-12 11:15:01 +0300

    api changed

--- app/src/main/java/com/example/fiveletters/data/model/Word.kt
+++ app/src/main/java/com/example/fiveletters/data/model/Word.kt
@@ -1,10 +1,8 @@
 package com.example.fiveletters.data.model
 
-import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class Word(
-    @SerialName("id") val id: Int,
-    @SerialName("word") val word: String,
+    val word: String,
 )
